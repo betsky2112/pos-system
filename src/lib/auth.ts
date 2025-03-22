c5494cc38e1df27fe9aed915e7455888ae3b74bc
betsky2112 <roberthussebastians@gmail.com>
2025-03-22 11:05:16 +0700

    Implement authentication middleware and JWT handling; update Prisma schema and add necessary dependencies

---
+++ src/lib/auth.ts
@@ -0,0 +1,56 @@
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import {cookies} from 'next/headers'
+
+export type JWTPayload = {
+	id: string
+	email: string
+	name: string
+	role: string
+}
+
+export const hashPassword = async (password: string) => {
+	return await bcrypt.hash(password, 10)
+}
+
+export const comparePassword = async (
+	plainPassword: string,
+	hashedPassword: string
+) => {
+	return await bcrypt.compare(plainPassword, hashedPassword)
+}
+
+export const generateJwtToken = (payload: JWTPayload) => {
+	return jwt.sign(payload, process.env.NEXTAUTH_SECRET as string, {
+		expiresIn: '7d',
+	})
+}
+
+export const verifyJwtToken = (token: string) => {
+	return jwt.verify(
+		token,
+		process.env.NEXTAUTH_SECRET as string
+	) as JWTPayload
+}
+
+export const setAuthCookie = async (token: string) => {
+	const cookieStore = await cookies()
+	cookieStore.set({
+		name: 'token',
+		value: token,
+		httpOnly: true,
+		path: '/',
+		secure: process.env.NEXTAUTH_SECRET === 'production',
+		maxAge: 60 * 60 * 24 * 7,
+	})
+}
+
+export const clearAuthCookie = async () => {
+	const cookieStore = await cookies()
+	cookieStore.delete('token')
+}
+
+export const getAuthToken = async () => {
+	const cookieStore = await cookies()
+	cookieStore.get('token')?.value
+}
---
+++ src/middleware.ts
@@ -0,0 +1,45 @@
+import {NextResponse} from 'next/server'
+import type {NextRequest} from 'next/server'
+import {verifyJwtToken} from './lib/auth'
+import {error} from 'node:console'
+
+export async function middleware(request: NextRequest) {
+	const {pathname} = request.nextUrl
+
+	if (
+		pathname === '/login' ||
+		pathname === '/register' ||
+		pathname.startsWith('/_next') ||
+		pathname.startsWith('/api/auth')
+	) {
+		return NextResponse.next()
+	}
+
+	const token = request.cookies.get('token')?.value
+
+	if (!token) {
+		return NextResponse.redirect(new URL('/login', request.url))
+	}
+
+	try {
+		const payload = await verifyJwtToken(token)
+
+		// jika user bukan admin dan mencoba mengakses halaman admin
+		if (
+			payload.role !== 'ADMIN' &&
+			(pathname.startsWith('/admin') || pathname.startsWith('/api/admin'))
+		) {
+			return NextResponse.redirect(new URL('/dashboard', request.url))
+		}
+		return NextResponse.next()
+	} catch {
+		error
+	}
+	{
+		return NextResponse.redirect(new URL('/login', request.url))
+	}
+}
+
+export const config = {
+	matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico).*)'],
+}
